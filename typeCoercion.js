94ec52792958544a2af7af4ed68c8544a576e757
Saumya Singh <developersaumya@gmail.com>
2025-01-23 17:21:59 +0530

    JS type coercion

---
+++ typeCoercion.js
@@ -0,0 +1,17 @@
+// const result = "5" + 3;
+// console.log(result);         
+// console.log(typeof result);
+
+// const result = "10" - 5;
+// console.log(result);         
+// console.log(typeof result);  
+
+
+// const result = true + 1;
+// console.log(result);         
+// console.log(typeof result);  
+
+
+const result = null + 1;
+console.log(result);         
+console.log(typeof result);
