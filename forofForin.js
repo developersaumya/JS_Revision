74430fc9795c394ec25d34598bf56f6023d8fa91
Saumya Singh <developersaumya@gmail.com>
2025-01-24 13:39:56 +0530

    for Of & for in Javascript

---
+++ forofForin.js
@@ -0,0 +1,14 @@
+const obj ={
+    channel:"explocode",
+    platform:"instagram"
+}
+
+const arr = ["PHP",".NET","JAVA"]
+
+for (const n of obj) {
+    console.log(n);
+}
+
+// for (const n of arr) {
+//     console.log(n);
+// }
