6f4030929c72c30f2478547df99e6eeee79be50b
Saumya Singh <developersaumya@gmail.com>
2025-01-25 13:03:11 +0530

    Basics in Javascript

--- constObject.js
+++ constObject.js
@@ -1,20 +1,23 @@
 //MODIFY CONST OBJECT
 
-const channel = "explocode";
-channel = "SOMETHING NEW";
-console.log(channel);
+// const channel = "explocode";
+// channel = "SOMETHING NEW";
+// console.log(channel);
 
 
-const data= {
-    name:"explocode"
+// const data= {
+//     name:"explocode"
+// }
+// data.name="something"
+// data.city="GGN"
+
+// console.log(data)
+
+
+data= {
+    city:"Delhi"
 }
-data.name="something"
-data.city="GGN"
-
 console.log(data)
-
-
-
 
 
 
